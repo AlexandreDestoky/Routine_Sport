f2cc0fbf64e24814cf998ec2b7993bf671d6e333
Destoky <adestoky@gmail.com>
2021-05-04 17:07:09 +0200

    Tableau de jour d'entrainements

---
+++ app.js
@@ -0,0 +1,47 @@
+let trainingDays = [
+  ["https://youtu.be/C8DbCZGj5KQ","https://youtu.be/c1mdorMppH4"],
+  ["https://youtu.be/SML-fA6EJh4","https://youtu.be/ldrG5GTgF_0"],
+  ["https://youtu.be/lyb7UAtsq4w","https://youtu.be/5oG8cMfKx9I"],
+  ["https://youtu.be/ISk3x9E7ars","https://youtu.be/qgjdta2jyxw"],
+  ["https://youtu.be/30c8d9smbsE","https://youtu.be/BKfGWNTssCY"], //5
+  ["https://youtu.be/MCvE258aPv0","https://youtu.be/CnfitL4Rl0o"],
+  ["https://youtu.be/oAPCPjnU1wA","https://youtu.be/igJrhW-GbNY"],
+  ["https://youtu.be/tCtS-hNs6rk","https://youtu.be/c1mdorMppH4"],
+  ["https://youtu.be/A3b5sXtf6rc","https://youtu.be/SML-fA6EJh4"],
+  ["https://youtu.be/ldrG5GTgF_0","https://youtu.be/-lO1pgoES74"], //10
+  ["https://youtu.be/lyb7UAtsq4w","https://youtu.be/5oG8cMfKx9I"],
+  ["https://youtu.be/GfUpbhaCK7Y","https://youtu.be/Irob_m12znc"],
+  ["https://youtu.be/eQKzCem0eUs","https://youtu.be/igJrhW-GbNY"],
+  ["https://youtu.be/PfATi3Tp5Go","https://youtu.be/c1mdorMppH4"],
+  ["https://youtu.be/C8DbCZGj5KQ","https://youtu.be/xngUfoWLkFw"], //15
+  ["https://youtu.be/mm47bCaCzpQ","https://youtu.be/lyb7UAtsq4w"],
+  ["https://youtu.be/ISk3x9E7ars","https://youtu.be/WRGCCvQwCyQ"],
+  ["https://youtu.be/30c8d9smbsE","https://youtu.be/qgjdta2jyxw"],
+  ["https://youtu.be/CnfitL4Rl0o","https://youtu.be/GfUpbhaCK7Y"],
+  ["https://youtu.be/HzjVKXKzpNY","https://youtu.be/BKfGWNTssCY"], //20
+  ["https://youtu.be/C8DbCZGj5KQ","https://youtu.be/C8DbCZGj5KQ"],
+  ["https://youtu.be/qgjdta2jyxw","https://youtu.be/c1mdorMppH4"],
+  ["https://youtu.be/SML-fA6EJh4","https://youtu.be/_q_-adYzkh0"],
+  ["https://youtu.be/ldrG5GTgF_0","https://youtu.be/-lO1pgoES74"],
+  ["https://youtu.be/5oG8cMfKx9I","https://youtu.be/lyb7UAtsq4w"], //25
+  ["https://youtu.be/ISk3x9E7ars","https://youtu.be/igJrhW-GbNY"],
+  ["https://youtu.be/lmgeM9Ne-Ns","https://youtu.be/BKfGWNTssCY"],
+  ["https://youtu.be/qgjdta2jyxw","https://youtu.be/eQKzCem0eUs"],
+  ["https://youtu.be/oAPCPjnU1wA","https://youtu.be/C8DbCZGj5KQ"],
+  ["https://youtu.be/30c8d9smbsE","https://youtu.be/mm47bCaCzpQ"],//30
+  ["https://youtu.be/CnfitL4Rl0o","https://youtu.be/ISk3x9E7ars"],
+  ["https://youtu.be/5oG8cMfKx9I","https://youtu.be/lyb7UAtsq4w"],
+  ["https://youtu.be/tCtS-hNs6rk","https://youtu.be/qgjdta2jyxw"],
+  ["https://youtu.be/Auo8veVyRIY","https://youtu.be/BKfGWNTssCY"],
+  ["https://youtu.be/lmgeM9Ne-Ns","https://youtu.be/c1mdorMppH4"], //35
+  ["https://youtu.be/Irob_m12znc","https://youtu.be/qgjdta2jyxw"],
+  ["https://youtu.be/C8DbCZGj5KQ","https://youtu.be/C8DbCZGj5KQ"],
+  ["https://youtu.be/tCtS-hNs6rk","https://youtu.be/MCvE258aPv0"],
+  ["https://youtu.be/ISk3x9E7ars"," https://youtu.be/ISk3x9E7ars"],
+  ["https://youtu.be/30c8d9smbsE","https://youtu.be/30c8d9smbsE"], //40
+  ["https://youtu.be/eQKzCem0eUs","https://youtu.be/c1mdorMppH4"],
+  ["https://youtu.be/oAPCPjnU1wA","https://youtu.be/Auo8veVyRIY"],
+  ["https://youtu.be/C8DbCZGj5KQ","https://youtu.be/Vfv084H00QI"],
+  ["https://youtu.be/tCtS-hNs6rk","https://youtu.be/GflOdkA5nS0"],
+  ["https://youtu.be/xngUfoWLkFw","https://youtu.be/BKfGWNTssCY"],//45
+]
